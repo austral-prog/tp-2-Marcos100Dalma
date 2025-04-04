33c7a760847b05a90669e3aa79b4c640c47a97fc
Marcos100Dalma <mdalma@mail.austral.edu.ar>
2025-04-04 11:33:07 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -4,5 +4,5 @@ def earth():
     str(x > y)
     checkeo1 = str(x < y)
     checkeo2 = str(x > y)
-    print (f"The result of {x} comes first in the dictionary tnan {y} is {checkeo1}.")
-    print(f"The result of {y} comes first in the dictionary tnan {x} is {checkeo2}.")
+    print (f"The result of {x} comes first in the dictionary than {y} is {checkeo1}.")
+    print(f"The result of {y} comes first in the dictionary than {x} is {checkeo2}.")
